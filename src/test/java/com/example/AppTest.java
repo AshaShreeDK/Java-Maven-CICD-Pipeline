0a82add63c2c6c387b6dc1b60dfde437e322c871
AshaShreeDK <dlrashadk@gmail.com>
2025-09-20 01:52:49 +0530

    Added initial Maven app code

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testAppPass() {
+        // This test will always pass ✅
+        assertTrue(true, "This test should always pass!");
+    }
+
+  }
